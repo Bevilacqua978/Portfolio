17bb23885557cb5906677eb3197ed981d2c9656a
Michael Drinkwater <md91788n@pace.edu>
2019-10-10 22:07:52 -0400

    Add project files.

---
+++ PortfolioNET/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PortfolioNET
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/lib/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/lib/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/lib/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/lib/bootstrap.js", "~/Scripts/Site.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/lib/bootstrap.css",
+                      "~/Content/css/site.css", "~/Content/css/Output/styles.css", "~/Content/css/lib/portfolio.css"));
+        }
+    }
+}
---
+++ PortfolioNET/Controllers/WorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PortfolioNET.Models;
+
+namespace PortfolioNET.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult Work()
+        {
+            var work = new Work() { Name = "My Work" };
+
+            return View(work);
+        }
+    }
+}
